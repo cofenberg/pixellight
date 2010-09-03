3eacf25507772f0995c62c82579fb98ef07473cd
Christian Ofenberg <c.ofenberg@pixellight.org>
2010-09-03 14:59:34 +0200

    "newton.dll" (Newton), "ode.dll" (ODE) and "PhysXLoader.dll" (PhysX) are now copied by CMake Check/Install into the runtime directory

--- External/Recommended/Newton/Check.cmake
+++ External/Recommended/Newton/Check.cmake
@@ -14,6 +14,12 @@ if(NOT EXISTS ${CMAKETOOLS_CURRENT_BUILT_FILE})
 
 	# Unpack the package
 	external_check_unpack()
+
+	# For Windows, copy the required dlls into the runtime directory
+	if(WIN32)
+		external_copy("${CMAKETOOLS_CURRENT_DEST_DIR}/bin/newton.dll" "${PL_BIN_DIR}/../Bin/PLRuntime/Plugins/PLPhysics")	# Bin
+		external_copy("${CMAKETOOLS_CURRENT_DEST_DIR}/bin/newton.dll" "${PL_BIN_DIR}/PLRuntime/Plugins/PLPhysics")			# Bin-Windows
+	endif()
 endif()
 
 # Done
