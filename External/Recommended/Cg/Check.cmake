8badcf064147e9f377fe97287728206ab42d5b15
Christian Ofenberg <c.ofenberg@pixellight.org>
2010-09-03 12:47:26 +0200

    "cg.dll", "cgGL.dll" and "cgD3D9.dll" are now copied by the CMake install script into the runtime directory

--- External/Recommended/Cg/Check.cmake
+++ External/Recommended/Cg/Check.cmake
@@ -14,6 +14,16 @@ if(NOT EXISTS ${CMAKETOOLS_CURRENT_BUILT_FILE})
 
 	# Unpack the package
 	external_check_unpack()
+
+	# For Windows, copy the required dlls into the runtime directory
+	if(WIN32)
+		external_copy("${CMAKETOOLS_CURRENT_DEST_DIR}/bin/cg.dll"     "${PL_BIN_DIR}/../Bin/PLRuntime/Plugins/PLRenderer")	# Bin
+		external_copy("${CMAKETOOLS_CURRENT_DEST_DIR}/bin/cg.dll"     "${PL_BIN_DIR}/PLRuntime/Plugins/PLRenderer")			# Bin-Windows
+		external_copy("${CMAKETOOLS_CURRENT_DEST_DIR}/bin/cgGL.dll"   "${PL_BIN_DIR}/../Bin/PLRuntime/Plugins/PLRenderer")	# Bin
+		external_copy("${CMAKETOOLS_CURRENT_DEST_DIR}/bin/cgGL.dll"   "${PL_BIN_DIR}/PLRuntime/Plugins/PLRenderer")			# Bin-Windows
+		external_copy("${CMAKETOOLS_CURRENT_DEST_DIR}/bin/cgD3D9.dll" "${PL_BIN_DIR}/../Bin/PLRuntime/Plugins/PLRenderer")	# Bin
+		external_copy("${CMAKETOOLS_CURRENT_DEST_DIR}/bin/cgD3D9.dll" "${PL_BIN_DIR}/PLRuntime/Plugins/PLRenderer")			# Bin-Windows
+	endif()
 endif()
 
 # Done
