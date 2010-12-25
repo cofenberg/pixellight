5a10b8fde8cf568c92e8882d8e54e1ccb4c67d3f
Christian Ofenberg <c.ofenberg@pixellight.org>
2010-12-25 13:21:02 +0100

    MSVC9 -> MSVC10

--- Packages/Pack-Runtime.cmake
+++ Packages/Pack-Runtime.cmake
@@ -9,7 +9,6 @@ if(EXISTS "${CMAKE_ROOT}/Modules/CPack.cmake")
 	set(CPACK_PL_PACKAGE_PATH	${CMAKE_SOURCE_DIR}/Packages)
 	if(MSVC10)
 		# Microsoft Visual C++ 2010 Redistributable
-		set(CPACK_PL_VC_FILE	"x86_Microsoft.VC90.CRT_1fc8b3b9a1e18e3b_9.0.21022.8_x-ww_d08d0375")	# [TODO] Update to VC10
 		set(CPACK_PL_VC_MESSAGE	"In order to install PixelLight on your computer, you will need the Microsoft Visual C++ 2010 Redestributable package. Do you want to install it now?")
 		set(CPACK_PL_VC_REDIST	"VC10\\\\vcredist_x86.exe")
 	endif()
