a37d08bed76d2acda66cb59453c620d3ba4bb4e2
Stephan Wezel <s.wezel@web.de>
2011-01-08 01:56:13 +0100

    set linux compiler optimization flags only when building a release build

--- cmake/Platforms/LinuxGCC.cmake
+++ cmake/Platforms/LinuxGCC.cmake
@@ -56,22 +56,29 @@ endif()
 ## Compiler flags 
 ##################################################
 
+# Check if an 32Bit build should be made on an 64Bit host (CMAKE_SIZEOF_VOID_P has the value 8 on 64Bit Systems at least on x86 systems )
+if ((CMAKETOOLS_TARGET_BITSIZE MATCHES 32) AND (CMAKE_SIZEOF_VOID_P MATCHES 8))
+	message(STATUS "Add compiler and linker flags for 32Bit on 64Bit host")
+	set(LINUX_COMPILE_FLAGS
+		${LINUX_COMPILE_FLAGS}
+		-m32
+	)
+	
+	set(LINUX_LINKER_FLAGS
+		${LINUX_LINKER_FLAGS}
+		-m32
+	)
+endif()
+
 # Compiler flags
 set(LINUX_COMPILE_FLAGS
 	-pthread												# Use pthreads
 	-fno-rtti												# No C++ RTTI
 	-fno-exceptions											# No C++ exception handling
 	-pipe													# Use Pipes
-	-O3														# Very aggressive transformations (long compile times, but usally best runtime performance!)
-	-msse3													# Use SSE3 instruction set
+	-msse3													# Use SSE3 instruction set / code might not run on CPUs with no sse3 instruction
 	# The following flag usage is basing on information from http://developer.amd.com/documentation/articles/pages/Compiler-FlagDrivenPerformanceGains.aspx
-	-fomit-frame-pointer									# Don't keep the frame pointer in a register for functions that don't need one
-	-funroll-all-loops										# Perform the optimization of loop unrolling
 	-ffast-math												# Perform floating point transformations that may break IEEE/ISO rules regarding floating point arithmetic
-	-fpeel-loops											# Peels the loops for that there is enough information that they do not roll much (from profile feedback)
-	-ftree-vectorize										# Enable the vectorizer
-	# The following flag usage is basing on information from http://software.intel.com/en-us/forums/showthread.php?t=66902
-	--param max-unroll-times=4
 )
 if(CMAKE_BUILD_TYPE MATCHES Debug)
 	##################################################
@@ -88,5 +95,13 @@ else()
 	##################################################
 	set(LINUX_COMPILE_FLAGS
 		${LINUX_COMPILE_FLAGS}
+		-O3														# Very aggressive transformations (long compile times, but usally best runtime performance!)
+		# The following flag usage is basing on information from http://developer.amd.com/documentation/articles/pages/Compiler-FlagDrivenPerformanceGains.aspx
+		-fomit-frame-pointer									# Don't keep the frame pointer in a register for functions that don't need one
+		-funroll-all-loops										# Perform the optimization of loop unrolling
+		-fpeel-loops											# Peels the loops for that there is enough information that they do not roll much (from profile feedback)
+		-ftree-vectorize										# Enable the vectorizer
+		# The following flag usage is basing on information from http://software.intel.com/en-us/forums/showthread.php?t=66902
+		--param max-unroll-times=4
 	)
 endif()
