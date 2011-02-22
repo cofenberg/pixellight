716c2b16500c9380ea0d43e2c738cb66b4a42aec
Christian Ofenberg <c.ofenberg@pixellight.org>
2011-02-22 18:55:50 +0100

    Added missing PLDemoSPARK_PL files...

---
+++ PLDemos/PLDemoSPARK_PL/src/SNBasicDemo.h
@@ -0,0 +1,107 @@
+/*********************************************************\
+ *  File: SNBasicDemo.h                                  *
+ *
+ *  Copyright (C) 2002-2011 The PixelLight Team (http://www.pixellight.org/)
+ *
+ *  This file is part of PixelLight.
+ *
+ *  PixelLight is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Lesser General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  PixelLight is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public License
+ *  along with PixelLight. If not, see <http://www.gnu.org/licenses/>.
+\*********************************************************/
+
+
+#ifndef __PLDEMOSPARK_PL_SNBASICDEMO_H__
+#define __PLDEMOSPARK_PL_SNBASICDEMO_H__
+#pragma once
+
+
+//[-------------------------------------------------------]
+//[ Includes                                              ]
+//[-------------------------------------------------------]
+#include <SPARK_PL/PLSceneNodes/SNSystem.h>
+
+
+//[-------------------------------------------------------]
+//[ Forward declarations                                  ]
+//[-------------------------------------------------------]
+namespace SPK {
+	class Group;
+	class Model;
+}
+
+
+//[-------------------------------------------------------]
+//[ Classes                                               ]
+//[-------------------------------------------------------]
+/**
+*  @brief
+*    Basic SPARK particle system demo scene node
+*
+*  @note
+*    - Basing on "SPARK Basic Demo"
+*/
+class SNBasicDemo : public SPARK_PL::SNSystem {
+
+
+	//[-------------------------------------------------------]
+	//[ RTTI interface                                        ]
+	//[-------------------------------------------------------]
+	pl_class(pl_rtti_export, SNBasicDemo, "", SPARK_PL::SNSystem, "Basic SPARK particle system demo scene node")
+		pl_constructor_0(DefaultConstructor, "Default constructor", "")
+	pl_class_end
+
+
+	//[-------------------------------------------------------]
+	//[ Public functions                                      ]
+	//[-------------------------------------------------------]
+	public:
+		/**
+		*  @brief
+		*    Default constructor
+		*/
+		SNBasicDemo();
+
+		/**
+		*  @brief
+		*    Destructor
+		*/
+		virtual ~SNBasicDemo();
+
+
+	//[-------------------------------------------------------]
+	//[ Protected virtual SNSystem functions                  ]
+	//[-------------------------------------------------------]
+	protected:
+		virtual void Update();
+
+
+	//[-------------------------------------------------------]
+	//[ Private virtual PLScene::SceneNode functions          ]
+	//[-------------------------------------------------------]
+	private:
+		virtual void InitFunction();
+
+
+	//[-------------------------------------------------------]
+	//[ Private data                                          ]
+	//[-------------------------------------------------------]
+	private:
+		SPK::Group *m_pParticleGroup;	/**< SPARK particle group, can be a null pointer */
+		SPK::Model *m_pParticleModel;
+		float		m_fStep;
+
+
+};
+
+
+#endif // __PLDEMOSPARK_PL_SNBASICDEMO_H__
---
+++ PLDemos/PLDemoSPARK_PL/src/SNGravitationDemo.h
@@ -0,0 +1,118 @@
+/*********************************************************\
+ *  File: SNGravitationDemo.h                            *
+ *
+ *  Copyright (C) 2002-2011 The PixelLight Team (http://www.pixellight.org/)
+ *
+ *  This file is part of PixelLight.
+ *
+ *  PixelLight is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Lesser General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  PixelLight is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public License
+ *  along with PixelLight. If not, see <http://www.gnu.org/licenses/>.
+\*********************************************************/
+
+
+#ifndef __PLDEMOSPARK_PL_SNGRAVITATIONDEMO_H__
+#define __PLDEMOSPARK_PL_SNGRAVITATIONDEMO_H__
+#pragma once
+
+
+//[-------------------------------------------------------]
+//[ Includes                                              ]
+//[-------------------------------------------------------]
+#include <SPARK_PL/PLSceneNodes/SNSystem.h>
+
+
+//[-------------------------------------------------------]
+//[ Forward declarations                                  ]
+//[-------------------------------------------------------]
+namespace SPK {
+	class Group;
+	class Model;
+	class PointMass;
+}
+
+
+//[-------------------------------------------------------]
+//[ Classes                                               ]
+//[-------------------------------------------------------]
+/**
+*  @brief
+*    Gravitation SPARK particle system demo scene node
+*
+*  @note
+*    - Basing on "SPARK Gravitation Demo" from SPARK
+*/
+class SNGravitationDemo : public SPARK_PL::SNSystem {
+
+
+	//[-------------------------------------------------------]
+	//[ RTTI interface                                        ]
+	//[-------------------------------------------------------]
+	pl_class(pl_rtti_export, SNGravitationDemo, "", SPARK_PL::SNSystem, "Gravitation SPARK particle system demo scene node")
+		pl_constructor_0(DefaultConstructor, "Default constructor", "")
+	pl_class_end
+
+
+	//[-------------------------------------------------------]
+	//[ Public functions                                      ]
+	//[-------------------------------------------------------]
+	public:
+		/**
+		*  @brief
+		*    Default constructor
+		*/
+		SNGravitationDemo();
+
+		/**
+		*  @brief
+		*    Destructor
+		*/
+		virtual ~SNGravitationDemo();
+
+
+	//[-------------------------------------------------------]
+	//[ Protected virtual SNSystem functions                  ]
+	//[-------------------------------------------------------]
+	protected:
+		virtual void Update();
+
+
+	//[-------------------------------------------------------]
+	//[ Private virtual PLScene::SceneNode functions          ]
+	//[-------------------------------------------------------]
+	private:
+		virtual void InitFunction();
+
+
+	//[-------------------------------------------------------]
+	//[ Private static data                                   ]
+	//[-------------------------------------------------------]
+	private:
+		// The number of point masses
+		static const PLGeneral::uint32 NumOfPointMasses = 2;
+
+
+	//[-------------------------------------------------------]
+	//[ Private data                                          ]
+	//[-------------------------------------------------------]
+	private:
+		SPK::Group		*m_pParticleGroup;	/**< SPARK particle group, can be a null pointer */
+		SPK::Group		*m_pMassGroup;		/**< SPARK mass group, can be a null pointer */
+		SPK::Model		*m_pParticleModel;
+		SPK::PointMass	*m_pPointMasses[NumOfPointMasses];
+		float			 m_fStep;
+
+
+};
+
+
+#endif // __PLDEMOSPARK_PL_SNGRAVITATIONDEMO_H__
