bfb35c62ba70fd3c57ef79ccb3b326320a357ab7
Christian Ofenberg <c.ofenberg@pixellight.org>
2011-04-17 17:27:09 +0200

    Autodesk 3ds Max 2012 scene exporter added

---
+++ External/Optional/Max2012/Configure.cmake
@@ -0,0 +1,13 @@
+##################################################
+## Project
+##################################################
+include(${CMAKETOOLS_DIR}/External/Configure.cmake)
+
+##################################################
+## Configure
+##################################################
+
+# There are no public 3ds Max 2012 SDK sources...
+
+# Done
+external_configure_done()
