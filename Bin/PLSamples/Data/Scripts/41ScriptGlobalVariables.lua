5d5cbd7e184cffe5a9b922d9d892f5c8f347b526
Christian Ofenberg <c.ofenberg@pixellight.org>
2011-06-23 11:18:44 +0200

    Work on the script documentation

--- Bin/PLSamples/Data/Scripts/41ScriptGlobalVariables.lua
+++ Bin/PLSamples/Data/Scripts/41ScriptGlobalVariables.lua
@@ -5,5 +5,7 @@ g_Factor = 1
 g_Name = "Bob"
 
 -- Global variables within a namespace
-PublicVariables = {};		-- Create the "PublicVariables"-namespace
-PublicVariables.Health = 42	-- Create the variable "Health" within the "PublicVariables"-namespace
+PublicVariables = {	-- Create the "PublicVariables"-namespace
+	Health	= 42,	-- Create the variable "Health" within the "PublicVariables"-namespace
+	Gold	= 21,	-- Create the variable "Gold" within the "PublicVariables"-namespace
+}
