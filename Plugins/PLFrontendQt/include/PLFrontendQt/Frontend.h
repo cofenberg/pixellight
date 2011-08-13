9b4ca72913b461e3e5a3d60ae4f4d23a4105a363
Stephan Wezel <s.wezel@web.de>
2011-08-13 20:26:04 +0200

    fixed toggeling show/hide mouse cursor in the Qt frontend

--- Plugins/PLFrontendQt/include/PLFrontendQt/Frontend.h
+++ Plugins/PLFrontendQt/include/PLFrontendQt/Frontend.h
@@ -137,6 +137,7 @@ class Frontend : public PLCore::FrontendImpl {
 		bool	 m_bFullscreenAltTab;		/**< Is it allowed to use Alt-Tab within fullscreen mode? */
 		bool	 m_bIsFullscreen;			/**< 'true' if the window is in fullscreen mode, else 'false' */
 		QCursor *m_pQCursorBlank;			/**< Blank (invisible) mouse cursor, can be a null pointer */ 
+		bool	 m_bMouseVisible;			/**< Is the mouse cursor visible? */
 
 
 };
