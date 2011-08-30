e1fd414253cc0c742a80dfa3f570689a8f1f3973
Christian Ofenberg <c.ofenberg@pixellight.org>
2011-08-30 15:46:41 +0200

    Android port related: "PLCoreLinux.cpp": The Android NDK has no support for "wcstof()"

--- Base/PLCore/src/PLCoreLinux.cpp
+++ Base/PLCore/src/PLCoreLinux.cpp
@@ -939,6 +939,91 @@ long _wtol( const wchar_t *str )
 	return static_cast<int>(wcstol(str, 0, 10));
 }
 
+#ifdef ANDROID
+	// Even if there's a "wchar.h"-header, wchar_t is officially not supported by Android
+	// (no problem, wchar_t is for Windows, UTF-8 for Linux and the string class handles both as well as ASCII)
+
+	// Source: http://www.opensource.apple.com/source/Libc/Libc-320/locale/FreeBSD/wcstof.c
+	// -> Minor adjustments to make it compile
+	/*-
+	 * Copyright (c) 2002, 2003 Tim J. Robbins
+	 * All rights reserved.
+	 *
+	 * Redistribution and use in source and binary forms, with or without
+	 * modification, are permitted provided that the following conditions
+	 * are met:
+	 * 1. Redistributions of source code must retain the above copyright
+	 *    notice, this list of conditions and the following disclaimer.
+	 * 2. Redistributions in binary form must reproduce the above copyright
+	 *    notice, this list of conditions and the following disclaimer in the
+	 *    documentation and/or other materials provided with the distribution.
+	 *
+	 * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
+	 * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+	 * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+	 * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
+	 * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+	 * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+	 * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+	 * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+	 * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+	 * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+	 * SUCH DAMAGE.
+	 */
+	#include <stdlib.h>
+	float wcstof(const wchar_t *nptr, wchar_t **endptr)
+	{
+		float val;
+		char *buf, *end, *p;
+		const wchar_t *wcp;
+		size_t clen, len;
+
+		while (iswspace(*nptr))
+			nptr++;
+
+		wcp = nptr;
+		{
+			mbstate_t state;
+			if ((len = wcsrtombs(NULL, &wcp, 0, &state)) == (size_t)-1) {
+				if (endptr != NULL)
+					*endptr = (wchar_t *)nptr;
+				return (0.0);
+			}
+		}
+		if ((buf = (char*)malloc(len + 1)) == NULL)
+			return (0.0);
+		{
+			mbstate_t state;
+			wcsrtombs(buf, &wcp, len + 1, &state);
+		}
+
+		val = strtof(buf, &end);
+
+		if (endptr != NULL) {
+	#if 1					/* Fast, assume 1:1 WC:MBS mapping. */
+			*endptr = (wchar_t *)nptr + (end - buf);
+			(void)clen;
+			(void)p;
+	#else					/* Slow, conservative approach. */
+			*endptr = (wchar_t *)nptr;
+			p = buf;
+			{
+				mbstate_t state;
+				while (p < end &&
+					(clen = mbrlen(p, end - p, &state)) > 0) {
+					p += clen;
+					(*endptr)++;
+				}
+			}
+	#endif
+		}
+
+		free(buf);
+
+		return (val);
+	}
+#endif
+
 float _wtof( const wchar_t *str )
 {
 	return wcstof(str,0);
