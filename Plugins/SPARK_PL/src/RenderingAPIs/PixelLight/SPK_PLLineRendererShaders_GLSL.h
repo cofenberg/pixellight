a0b49038f248020f4b45369e211a7749d58b0c92
Christian Ofenberg <cofenberg@co.(none)>
2011-09-15 14:19:54 +0200

    highp -> mediump

--- Plugins/SPARK_PL/src/RenderingAPIs/PixelLight/SPK_PLLineRendererShaders_GLSL.h
+++ Plugins/SPARK_PL/src/RenderingAPIs/PixelLight/SPK_PLLineRendererShaders_GLSL.h
@@ -23,18 +23,18 @@
 // GLSL (OpenGL 2.0 ("#version 110") and OpenGL ES 2.0 ("#version 100")) vertex shader source code, "#version" is added by hand
 static const PLCore::String sSPK_PLLineRendererShaders_GLSL_VS = "\
 // Attributes\n\
-attribute highp vec3 VertexPosition;	// Object space vertex position input\n\
-attribute lowp  vec4 VertexColor;		// Vertex color input\n\
-varying   lowp  vec4 VertexColorVS;		// Vertex color output\n\
+attribute mediump vec3 VertexPosition;	// Object space vertex position input\n\
+attribute lowp    vec4 VertexColor;		// Vertex color input\n\
+varying   lowp    vec4 VertexColorVS;	// Vertex color output\n\
 \n\
 // Uniforms\n\
-uniform highp mat4 ObjectSpaceToClipSpaceMatrix;	// Object space to clip space matrix\n\
+uniform mediump mat4 ObjectSpaceToClipSpaceMatrix;	// Object space to clip space matrix\n\
 \n\
 // Programs\n\
 void main()\n\
 {\n\
 	// Calculate the clip space vertex position, lower/left is (-1,-1) and upper/right is (1,1)\n\
-	gl_Position = ObjectSpaceToClipSpaceMatrix*vec4(VertexPosition, 1.0);\n\
+	gl_Position = ObjectSpaceToClipSpaceMatrix*(mediump vec4(VertexPosition, 1.0));\n\
 \n\
 	// Pass through the vertex color\n\
 	VertexColorVS = VertexColor;\n\
--- Plugins/SPARK_PL/src/RenderingAPIs/PixelLight/SPK_PLPointRendererShaders_GLSL.h
+++ Plugins/SPARK_PL/src/RenderingAPIs/PixelLight/SPK_PLPointRendererShaders_GLSL.h
@@ -23,24 +23,24 @@
 // GLSL (OpenGL 2.1 ("#version 120") and OpenGL ES 2.0 ("#version 100")) vertex shader source code, "#version" is added by hand
 static const PLCore::String sSPK_PLPointRendererShaders_GLSL_VS = "\
 // Attributes\n\
-attribute highp vec3 VertexPosition;	// Object space vertex position input\n\
-attribute lowp  vec4 VertexColor;		// Vertex color input\n\
-varying   lowp  vec4 VertexColorVS;		// Vertex color output\n\
+attribute mediump vec3 VertexPosition;	// Object space vertex position input\n\
+attribute lowp    vec4 VertexColor;		// Vertex color input\n\
+varying   lowp    vec4 VertexColorVS;	// Vertex color output\n\
 \n\
 // Uniforms\n\
-uniform highp mat4 ObjectSpaceToClipSpaceMatrix;	// Object space to clip space matrix\n\
-uniform highp vec4 PointParameters;					// Point parameters: point size, point scale a, point scale b, point scale c \n\
+uniform mediump mat4 ObjectSpaceToClipSpaceMatrix;	// Object space to clip space matrix\n\
+uniform mediump vec4 PointParameters;				// Point parameters: point size, point scale a, point scale b, point scale c \n\
 \n\
 // Programs\n\
 void main()\n\
 {\n\
 	// Calculate the clip space vertex position, lower/left is (-1,-1) and upper/right is (1,1)\n\
-	gl_Position = ObjectSpaceToClipSpaceMatrix*vec4(VertexPosition, 1.0);\n\
+	gl_Position = ObjectSpaceToClipSpaceMatrix*(mediump vec4(VertexPosition, 1.0));\n\
 \n\
 	// Calculate the point size (formulas from http://www.opengl.org/registry/specs/ARB/point_parameters.txt)\n\
-	float distance = -gl_Position.z;\n\
-	//    dist_atten = 1  /(a                 + b*distance                 + c*distance*distance)\n\
-	float dist_atten = 1.0/(PointParameters.y + PointParameters.z*distance + PointParameters.w*distance*distance);\n\
+	mediump float distance = -gl_Position.z;\n\
+	//            dist_atten = 1  /(a                 + b*distance                 + c*distance*distance)\n\
+	mediump float dist_atten = 1.0/(PointParameters.y + PointParameters.z*distance + PointParameters.w*distance*distance);\n\
 	// derived_size = Clamp(size * sqrt(dist_atten(dist)))\n\
 	gl_PointSize    = clamp(PointParameters.x*sqrt(dist_atten), 1.0, 1024.0);\n\
 \n\
