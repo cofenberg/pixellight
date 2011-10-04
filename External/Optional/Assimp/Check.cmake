5e7eb7c19b5c328a744fedc5970554793d7e75af
Stephan Wezel <s.wezel@web.de>
2011-10-04 19:52:37 +0200

    created static builds of the assimp lib

--- External/Optional/Assimp/Check.cmake
+++ External/Optional/Assimp/Check.cmake
@@ -52,10 +52,10 @@ if(NOT EXISTS ${CMAKETOOLS_CURRENT_BUILT_FILE})
 	elseif(LINUX)
 		if(CMAKETOOLS_TARGET_BITSIZE MATCHES "64")
 			# md5sum of the 64bit Linux package
-			set(md5sum "d740c4f50ca6889b093f37bec416b61f")
+			set(md5sum "03cb10e6f57195ef012d0fb97f253a52")
 		else()
 			# md5sum of the 32bit Linux package
-			set(md5sum "ba721e8316e0f9142ed795b1b4e61bdd")
+			set(md5sum "935569e74856513a2072a544c6127f85")
 		endif()
 	endif()
 	external_check_download("" ${md5sum})
