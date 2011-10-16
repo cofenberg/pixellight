dff807ac9fc2908bf62031e95b21c8fdd9e61d4d
Christian Ofenberg <c.ofenberg@pixellight.org>
2011-10-16 16:48:53 +0200

    Tests: Added "PixelLightAndroid", the project I used to develop the Android port

---
+++ Tests/PixelLightAndroid/src/AndroidMain.cpp
@@ -0,0 +1,51 @@
+/*********************************************************\
+ *  File: FrontendAndroid.cpp                            *
+ *
+ *  Copyright (C) 2002-2011 The PixelLight Team (http://www.pixellight.org/)
+ *
+ *  This file is part of PixelLight.
+ *
+ *  PixelLight is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Lesser General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  PixelLight is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public License
+ *  along with PixelLight. If not, see <http://www.gnu.org/licenses/>.
+\*********************************************************/
+
+
+//[-------------------------------------------------------]
+//[ Includes                                              ]
+//[-------------------------------------------------------]
+#include <PLFrontendOS/FrontendAndroid.h>
+
+
+//[-------------------------------------------------------]
+//[ Global functions                                      ]
+//[-------------------------------------------------------]
+/**
+*  @brief
+*    Program entry point
+*
+*  @remarks
+*    This is the main entry point of a native application that is using
+*    android_native_app_glue. It runs in its own thread, with its own
+*    event loop for receiving input events and doing other things.
+*/
+void android_main(struct android_app* state)
+{
+	// Make sure glue isn't stripped
+	app_dummy();
+
+	// Create a frontend instance on the C runtime stack
+	PLFrontendOS::FrontendAndroid cFrontendAndroid(*state);
+
+	// Finish the given activity
+	ANativeActivity_finish(state->activity);
+}
