6d9d1d8b89976b4b4756da54fab1da1537964b88
Christian Ofenberg <c.ofenberg@pixellight.org>
2011-11-11 20:08:13 +0100

    PLFrontendQt: Added comment for "ui_QPLDebugDialog.h" so that it's clear what's going and and why in here the include pattern suddenly is another one as everywhere else -> Did I remember the reason correctly?

--- Plugins/PLFrontendQt/src/QPLDebugDialog.cpp
+++ Plugins/PLFrontendQt/src/QPLDebugDialog.cpp
@@ -32,7 +32,7 @@
 #include "PLFrontendQt/DataModels/SceneGraphNodeTreeItemBase.h"
 #include "PLFrontendQt/QPLSceneContext.h"
 #include "PLFrontendQt/QPLDebugDialog.h"
-#include "ui_QPLDebugDialog.h"	// Automatically created by Qt's uic
+#include "ui_QPLDebugDialog.h"	// Automatically created by Qt's uic -> Do not write "PLFrontendQt/ui_QPLDebugDialog.h" because the resulting location of the automatically created file is not always up to up
 
 
 //[-------------------------------------------------------]
