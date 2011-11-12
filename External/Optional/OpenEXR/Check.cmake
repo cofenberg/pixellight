23eee47232f13e353c0650f34864bc8828c5cd98
Christian Ofenberg <c.ofenberg@pixellight.org>
2011-11-12 12:13:39 +0100

    "PLImageLoaderEXR": Updated x64 Visual Studio project settings

--- External/Optional/OpenEXR/Check.cmake
+++ External/Optional/OpenEXR/Check.cmake
@@ -35,8 +35,8 @@ if(NOT EXISTS ${CMAKETOOLS_CURRENT_BUILT_FILE})
 	set(md5sum "")
 	if(WIN32)
 		if(CMAKETOOLS_TARGET_BITSIZE MATCHES "64")
-			# md5sum of the 64bit Windows package [TODO]
-			set(md5sum "")
+			# md5sum of the 64bit Windows package
+			set(md5sum "95dfeada4ff6d28975cafb6ae667d7d4")
 		else()
 			# md5sum of the 32bit Windows package
 			set(md5sum "a6c5a1e7c6b50a2e7fc5e68c2075dff3")
