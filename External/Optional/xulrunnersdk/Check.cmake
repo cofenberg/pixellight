d8df4a377f3ca4783210ded098b74417fe87f834
Christian Ofenberg <c.ofenberg@pixellight.org>
2011-11-12 15:14:15 +0100

    "PLFrontendMozilla": Updated x64 Visual Studio project settings

--- External/Optional/xulrunnersdk/Check.cmake
+++ External/Optional/xulrunnersdk/Check.cmake
@@ -35,8 +35,8 @@ if(NOT EXISTS ${CMAKETOOLS_CURRENT_BUILT_FILE})
 	set(md5sum "")
 	if(WIN32)
 		if(CMAKETOOLS_TARGET_BITSIZE MATCHES "64")
-			# md5sum of the 64bit Windows package [TODO]
-			set(md5sum "")
+			# md5sum of the 64bit Windows package
+			set(md5sum "a95b31350e7c2520bea645afc12bd0c7")
 		else()
 			# md5sum of the 32bit Windows package
 			set(md5sum "a95b31350e7c2520bea645afc12bd0c7")
