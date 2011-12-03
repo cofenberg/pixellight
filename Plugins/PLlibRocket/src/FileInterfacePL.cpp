52a967cd7f4b14b9c24c30cbc5c437fdd8202d8e
Christian Ofenberg <c.ofenberg@pixellight.org>
2011-12-03 15:59:42 +0100

    Reviewed GCC warning ("warning: converting to non-pointer type ‘Rocket::Core::FileHandle {aka unsigned int}’ from NULL [-Wconversion-null]")

--- Plugins/PLlibRocket/src/FileInterfacePL.cpp
+++ Plugins/PLlibRocket/src/FileInterfacePL.cpp
@@ -73,7 +73,7 @@ Rocket::Core::FileHandle FileInterfacePL::Open(const Rocket::Core::String& path)
 	}
 
 	// Error!
-	return NULL;	// nullptr may cause some compiler issues in here
+	return static_cast<Rocket::Core::FileHandle>(0);	// nullptr may cause some compiler issues in here
 }
 
 void FileInterfacePL::Close(Rocket::Core::FileHandle file)
