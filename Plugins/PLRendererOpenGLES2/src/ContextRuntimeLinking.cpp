6f15a522fb7582eee6a6d80070a32221ec796b8a
Christian Ofenberg <c.ofenberg@pixellight.org>
2011-12-25 21:59:54 +0100

    "PLRendererOpenGLES2::Context::ChooseConfig()": I hope this fixes the "PLRendererOpenGLES2/src/Context.cpp:362:25: error: non-constant-expression cannot be narrowed from type 'uint32' (aka 'unsigned int') to 'EGLint' (aka 'int') in initializer list" Clang error Stephan reported

--- Plugins/PLRendererOpenGLES2/src/ContextRuntimeLinking.cpp
+++ Plugins/PLRendererOpenGLES2/src/ContextRuntimeLinking.cpp
@@ -123,8 +123,8 @@ EGLConfig ContextRuntimeLinking::ChooseConfig(uint32 nMultisampleAntialiasingSam
 	EGLint nConfigs = 0;
 	bool bChooseConfigCapitulated = false;
 	bool bMultisampleAntialiasing = false;
-	uint32 nMultisampleAntialiasingSampleBuffers = 0;
-	uint32 nMultisampleAntialiasingSamplesCurrent = nMultisampleAntialiasingSamples;
+	EGLint nMultisampleAntialiasingSampleBuffers = 0;
+	EGLint nMultisampleAntialiasingSamplesCurrent = static_cast<EGLint>(nMultisampleAntialiasingSamples);
 	do {
 		// Get the current multisample antialiasing settings
 		bMultisampleAntialiasing = (nMultisampleAntialiasingSamplesCurrent > 1);	// Multisample antialiasing with just one sample per per pixel isn't real multisample, is it? :D
