9330b88ff64f4563aa47ac64f7c76952c94a8bab
Christian Ofenberg <c.ofenberg@pixellight.org>
2012-01-08 22:16:18 +0100

    PLCore: "CoreApplication::OnInitPlugins()" & "FrontendApplication::OnInitPlugins()": If the application executable directory is not the same as the application startup directory, do also scan the application startup directory

--- Base/PLCore/include/PLCore/Application/CoreApplication.h
+++ Base/PLCore/include/PLCore/Application/CoreApplication.h
@@ -503,6 +503,8 @@ class CoreApplication : public Object, protected AbstractLifecycle {
 		*    - Scan for plugins in PixelLight runtime directory "Plugins/" recursively
 		*    - Scan for plugins in application executable directory non-recursively
 		*    - Scan for plugins in application executable directory "Plugins/" recursively
+		*    - If the application executable directory is not the same as the application startup directory, scan for plugins in application startup directory non-recursively
+		*    - If the application executable directory is not the same as the application startup directory, scan for plugins in application startup directory "Plugins/" recursively
 		*
 		*  @note
 		*    - Part of the application framework initialization function "OnStart()"
