f51f6b7eecbe58abe4ded9c07b47d7cc1802dd54
Christian Ofenberg <c.ofenberg@pixellight.org>
2012-01-17 15:02:06 +0100

    Scripts: Added missing copyright information

--- Bin/Samples/Data/Scripts/45ScriptApplication.lua
+++ Bin/Samples/Data/Scripts/45ScriptApplication.lua
@@ -1,3 +1,25 @@
+--/*********************************************************\
+-- *  File: 45ScriptApplication.lua                        *
+-- *
+-- *  Copyright (C) 2002-2012 The PixelLight Team (http://www.pixellight.org/)
+-- *
+-- *  This file is part of PixelLight.
+-- *
+-- *  PixelLight is free software: you can redistribute it and/or modify
+-- *  it under the terms of the GNU Lesser General Public License as published by
+-- *  the Free Software Foundation, either version 3 of the License, or
+-- *  (at your option) any later version.
+-- *
+-- *  PixelLight is distributed in the hope that it will be useful,
+-- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+-- *  GNU Lesser General Public License for more details.
+-- *
+-- *  You should have received a copy of the GNU Lesser General Public License
+-- *  along with PixelLight. If not, see <http://www.gnu.org/licenses/>.
+--\*********************************************************/
+
+
 -- Scripted stand-alone application -> This is a Lua port of the C++ PixelLight SDK sample "60Scene"
 -- -> The global variable "this" points to the C++ RTTI application class instance invoking the script (usually the same as "PL.GetApplication()")
 
--- Bin/Samples/Data/Scripts/ScriptsScene.js
+++ Bin/Samples/Data/Scripts/ScriptsScene.js
@@ -1,3 +1,25 @@
+/*********************************************************\
+*  File: ScriptsScene.js                                 *
+*
+*  Copyright (C) 2002-2012 The PixelLight Team (http://www.pixellight.org/)
+*
+*  This file is part of PixelLight.
+*
+*  PixelLight is free software: you can redistribute it and/or modify
+*  it under the terms of the GNU Lesser General Public License as published by
+*  the Free Software Foundation, either version 3 of the License, or
+*  (at your option) any later version.
+*
+*  PixelLight is distributed in the hope that it will be useful,
+*  but WITHOUT ANY WARRANTY; without even the implied warranty of
+*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+*  GNU Lesser General Public License for more details.
+*
+*  You should have received a copy of the GNU Lesser General Public License
+*  along with PixelLight. If not, see <http://www.gnu.org/licenses/>.
+\*********************************************************/
+
+
 // This script is called by the scene "Scripts.scene"
 // -> The global variable "this" points to the C++ RTTI scene node modifier class instance invoking the script
 
