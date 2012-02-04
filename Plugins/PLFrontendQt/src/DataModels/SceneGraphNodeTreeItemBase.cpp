f94ce9ab71a10af878a65a662d90de935907c653
Christian Ofenberg <c.ofenberg@pixellight.org>
2012-02-04 13:01:05 +0100

    Update to make it compile when using Visual Studio

--- Plugins/PLFrontendQt/src/DataModels/SceneGraphNodeTreeItemBase.cpp
+++ Plugins/PLFrontendQt/src/DataModels/SceneGraphNodeTreeItemBase.cpp
@@ -24,7 +24,7 @@
 //[ Includes                                              ]
 //[-------------------------------------------------------]
 #include "PLFrontendQt/DataModels/SceneGraphNodeTreeItemBase.h"
-#include <DataModels/SceneGraphTreeModel.h>
+#include "PLFrontendQt/DataModels/SceneGraphTreeModel.h"
 
 
 //[-------------------------------------------------------]
