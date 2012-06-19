c324cb4387b63c71a79bdcf4febf17fcbaead0a1
Christian Ofenberg <c.ofenberg@pixellight.org>
2012-06-19 20:47:06 +0200

    Volume renderer plugins: Added GDCM external which is required for PLLoaderDICOM_GDCM

---
+++ External/Optional/gdcm/Check.cmake
@@ -0,0 +1,60 @@
+#*********************************************************#
+#*  File: Check.cmake                                    *
+#*
+#*  Copyright (C) 2002-2012 The PixelLight Team (http://www.pixellight.org/)
+#*
+#*  This file is part of PixelLight.
+#*
+#*  PixelLight is free software: you can redistribute it and/or modify
+#*  it under the terms of the GNU Lesser General Public License as published by
+#*  the Free Software Foundation, either version 3 of the License, or
+#*  (at your option) any later version.
+#*
+#*  PixelLight is distributed in the hope that it will be useful,
+#*  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+#*  GNU Lesser General Public License for more details.
+#*
+#*  You should have received a copy of the GNU Lesser General Public License
+#*  along with PixelLight. If not, see <http://www.gnu.org/licenses/>.
+#*********************************************************#
+
+
+##################################################
+## Project
+##################################################
+include(${CMAKETOOLS_DIR}/External/Check.cmake)
+
+##################################################
+## Check
+##################################################
+
+# Check if built file exists
+if(NOT EXISTS ${CMAKETOOLS_CURRENT_BUILT_FILE})
+	# Download prebuilt package from the repository
+	set(md5sum "")
+	if(WIN32)
+		if(CMAKETOOLS_TARGET_BITSIZE MATCHES "64")
+			# md5sum of the 64bit Windows package
+			set(md5sum "8df266aaff74c6947386127c6d6ba5b8")
+		else()
+			# md5sum of the 32bit Windows package
+			set(md5sum "06cbe1c93706cdb35628919c0b4f2bde")
+		endif()
+	elseif(LINUX)
+		if(CMAKETOOLS_TARGET_BITSIZE MATCHES "64")
+			# md5sum of the 64bit Linux package [TODO]
+			set(md5sum "")
+		else()
+			# md5sum of the 32bit Linux package [TODO]
+			set(md5sum "")
+		endif()
+	endif()
+	external_check_download("" ${md5sum})
+
+	# Unpack the package
+	external_check_unpack()
+endif()
+
+# Done
+external_check_done()
---
+++ External/Optional/gdcm/Readme.txt
@@ -0,0 +1,4 @@
+﻿GDCM (Grassroots DICOM) 2.0.18
+- License: BSD license (see "Copyright.txt"-file)
+- Used by the plugin "PLLoaderDICOM_GDCM" to load in DICOM (Digital Imaging and Communications in Medicine) images (no additional external dependencies)
+- Download from: http://sourceforge.net/projects/gdcm/
