3a03f9407363f2e6866d078fb08475b0089735d1
Christian Ofenberg <c.ofenberg@pixellight.org>
2012-08-18 12:21:06 +0200

    Next stop: PixelLight 1.0.0, see diary for details

--- PixelLight/PixelLight.cmake
+++ PixelLight/PixelLight.cmake
@@ -101,7 +101,7 @@ if(LINUX)
 	endif()
 
 	# Detailed version information?
-	set(CMAKETOOLS_DETAILED_VERSION_INFORMATION "0" CACHE BOOL "Create detailed version information? If active, symlinks like \"libPLCore.so.0.9.12\" and \"libPLCore.so.0\" are created and also used directly within the created libraries and executables.")
+	set(CMAKETOOLS_DETAILED_VERSION_INFORMATION "0" CACHE BOOL "Create detailed version information? If active, symlinks like \"libPLCore.so.1.0.0\" and \"libPLCore.so.0\" are created and also used directly within the created libraries and executables.")
 endif()
 
 # Repository URL (where to download the external pre-built packages from)
@@ -126,9 +126,9 @@ endif()
 set(CMAKETOOLS_CONFIG_SUFFIX "" CACHE STRING "Library suffix")
 
 # Version
-set(CMAKETOOLS_PROJECT_VERSION_MAJOR    0)
-set(CMAKETOOLS_PROJECT_VERSION_MINOR    9)
-set(CMAKETOOLS_PROJECT_VERSION_PATCH    12)
+set(CMAKETOOLS_PROJECT_VERSION_MAJOR    1)
+set(CMAKETOOLS_PROJECT_VERSION_MINOR    0)
+set(CMAKETOOLS_PROJECT_VERSION_PATCH    0)
 set(CMAKETOOLS_PROJECT_VERSION_RELEASE "R1")
 if(CMAKETOOLS_CONFIG_NIGHTLY)
 	# [TODO] Add date to string (e.g. "nightly-20100101")
